c0beac49b88b6e077c00118f54e3c0eb880617f4
Shravan Kuchkula <Shravan.Kuchkula@gmail.com>
2020-01-12 12:19:06 -0500

    Added a way to stream the transformed data back to s3

--- street-easy/dags/street_easy.py
+++ street-easy/dags/street_easy.py
@@ -13,7 +13,7 @@ from airflow.operators import (StreetEasyOperator)
 default_args = {
     'owner': 'shravan',
     'start_date': datetime(2018, 1, 20),
-    'end_date': datetime(2018, 3, 30),
+    'end_date': datetime(2018, 2, 1),
     'depends_on_past': False,
     'provide_context': True,
 }
@@ -51,9 +51,11 @@ extract_and_transform_streeteasy_data = StreetEasyOperator(
     task_id = "extract_and_transform_streeteasy_data",
     dag=dag,
     aws_credentials_id = "aws_credentials",
+    aws_credentials_dest_id = "aws_credentials_dest",
     s3_bucket = "streeteasy-data-exercise",
-    #s3_key = "test.csv.gz"
-    s3_key = "inferred_users.{ds}.csv.gz"
+    s3_dest_bucket = "skuchkula-etl",
+    s3_key = "inferred_users.{ds}.csv.gz",
+    s3_dest_key = "valid_searches.{ds}.csv.gz",
 )
 
 end_operator = DummyOperator(task_id='End_Execution', dag=dag)
--- street-easy/plugins/operators/extract_and_transform_streeteasy.py
+++ street-easy/plugins/operators/extract_and_transform_streeteasy.py
@@ -8,19 +8,25 @@ import re
 from s3fs.core import S3FileSystem
 
 class StreetEasyOperator(BaseOperator):
-    template_fields = ("s3_key",)
+    template_fields = ("s3_key", "s3_dest_key",)
 
     @apply_defaults
     def __init__(self,
                  aws_credentials_id="",
+                 aws_credentials_dest_id="",
                  s3_bucket="",
+                 s3_dest_bucket="",
                  s3_key="",
+                 s3_dest_key="",
                  *args, **kwargs):
 
         super(StreetEasyOperator, self).__init__(*args, **kwargs)
         self.s3_bucket = s3_bucket
+        self.s3_dest_bucket = s3_dest_bucket
         self.s3_key = s3_key
+        self.s3_dest_key = s3_dest_key
         self.aws_credentials_id = aws_credentials_id
+        self.aws_credentials_dest_id = aws_credentials_dest_id
 
     def valid_searches(searches):
         # split on \\n-
@@ -52,22 +58,62 @@ class StreetEasyOperator(BaseOperator):
             return valid_searches
 
     def execute(self, context):
-        aws_hook = AwsHook(self.aws_credentials_id)
-        credentials = aws_hook.get_credentials()
         self.log.info("Executing StreetEasyOperator!!")
+
+        # get the aws hooks
+        aws_hook = AwsHook(self.aws_credentials_id)
+        aws_dest_hook = AwsHook(self.aws_credentials_dest_id)
+
+        # get the credentials for source and destination
+        credentials = aws_hook.get_credentials()
+        credentials_dest = aws_dest_hook.get_credentials()
+
+        # build the s3 source path
         rendered_key = self.s3_key.format(**context)
-        self.log.info("Rendered Key is {}".format(rendered_key))
         rendered_key_no_dashes = re.sub(r'-', '', rendered_key)
         self.log.info("Rendered Key no dashes {}".format(rendered_key_no_dashes))
         s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key_no_dashes)
-        #s3_path = "s3://{}/{}".format(self.s3_bucket, self.s3_key)
-        self.log.info("Process data from {}".format(s3_path))
+
+        # get a S3 file handle
         s3 = S3FileSystem(anon=False, key=credentials.access_key, secret=credentials.secret_key)
 
+        self.log.info("Extract data from {}".format(s3_path))
+        # stream data from s3 and transform it
         with s3.open(s3_path, mode='rb') as s3_file:
+            # read in the data from s3
             data = pd.read_csv(s3_file, compression='gzip', names=['user_id', 'searches'])
+
+            # transform the data
             data['valid_searches'] = data['searches'].apply(StreetEasyOperator.valid_searches)
+
             data['num_valid_searches'] = data['valid_searches'].apply(len)
+
+            # keep only searches
+            data = data[data.num_valid_searches > 0]
+
+            # remove original searches
+            data = data.drop(['searches'], axis=1)
+
+            # calculate avg_listings
+
+            # calculate type_of_search
 
             self.log.info("Total valid searches today are: {}".format(np.sum(data['num_valid_searches'])))
             self.log.info("Total users today are: {}".format(np.sum(data['num_valid_searches'] > 0)))
+
+        # build the s3 destination path
+        rendered_dest_key = self.s3_dest_key.format(**context)
+        rendered_dest_key_no_dashes = re.sub(r'-', '', rendered_dest_key)
+        self.log.info("Rendered Key no dashes {}".format(rendered_dest_key_no_dashes))
+        s3_dest_path = "s3://{}/{}".format(self.s3_dest_bucket, rendered_dest_key_no_dashes)
+
+        # get a S3 file handle for destination
+        s3_dest = S3FileSystem(anon=False, key=credentials_dest.access_key, secret=credentials_dest.secret_key)
+
+        self.log.info("Load data into {}".format(s3_dest_path))
+        # stream the transformed data into s3
+        with s3_dest.open(s3_dest_path, mode='wb') as s3_dest_file:
+            s3_dest_file.write(data.to_csv(None, index=False).encode())
+            self.log.info("Completed writing {}".format(data.shape))
+
+        self.log.info("ETL process completed")
